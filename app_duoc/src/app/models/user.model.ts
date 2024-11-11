392320728c2fc89bbb61a294a22bc6976f58e954
cata lagos <cat.lagosg@duocuc.cl>
2024-11-11 20:15:04 -0300

    areglos2

--- app_duoc/src/app/models/user.model.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    uid: string;
-    email: string;
-    password: string;
-    name: string;
-    role: 'user' | 'profesor';  // El campo 'role' define el rol del usuario (alumno o profesor)
-  }
-  
