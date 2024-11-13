026bedbb6eebbc68bf893acdc2ebf7cc2450bd04
catalinalagosgomez <139135660+catalinalagosgomez@users.noreply.github.com>
2024-11-13 13:06:43 -0300

    Revert "cambios"

--- src/app/pages/asistencia/asistencia.page.ts
+++ src/app/pages/asistencia/asistencia.page.ts
@@ -17,6 +17,53 @@ export class AsistenciaPage {
     'Base de datos': ['BD_1', 'BD_2', 'BD_3'],
     'Calidad': ['CAL_1', 'CAL_2', 'CAL_3']
   };  
+  alumnosPorSeccion: { [key: string]: any[] } = {
+    'PGY_1': [
+      { nombre: 'Carlos', presente: true },  
+      { nombre: 'Ana', presente: false },    
+      { nombre: 'Irene', presente: true }    
+    ],
+    'PGY_2': [
+      { nombre: 'Pedro', presente: false },  
+      { nombre: 'Lucía', presente: true },   
+      { nombre: 'Amelie', presente: false }  
+    ],
+    'PGY_3': [
+      { nombre: 'Sofía', presente: true },   
+      { nombre: 'David', presente: false },  
+      { nombre: 'Valentina', presente: false } 
+    ],
+    'BD_1': [
+      { nombre: 'Juan', presente: false },   
+      { nombre: 'María', presente: true },   
+      { nombre: 'Noemí', presente: false }   
+    ],
+    'BD_2': [
+      { nombre: 'Roberto', presente: true },  
+      { nombre: 'Claudia', presente: false }, 
+      { nombre: 'Lucas', presente: true }     
+    ],
+    'BD_3': [
+      { nombre: 'Jorge', presente: false },   
+      { nombre: 'Estefanía', presente: false }, 
+      { nombre: 'Joaquin', presente: true }   
+    ],
+    'CAL_1': [
+      { nombre: 'Fernando', presente: true }, 
+      { nombre: 'Camila', presente: false },  
+      { nombre: 'Bryan', presente: false }    
+    ],
+    'CAL_2': [
+      { nombre: 'Andrés', presente: false },  
+      { nombre: 'Valeria', presente: true },  
+      { nombre: 'Abigail', presente: false }  
+    ],
+    'CAL_3': [
+      { nombre: 'Gustavo', presente: true },  
+      { nombre: 'Elena', presente: false },   
+      { nombre: 'Benjamin', presente: true }  
+    ]
+  };
   
   alumnos: any[] = [];  
   mostrarCalendario: boolean = false;  
@@ -45,6 +92,12 @@ export class AsistenciaPage {
     this.alumnos = []; 
     console.log('Asignatura seleccionada:', this.asignatura);
     console.log('Secciones disponibles:', this.seccionesDisponibles);
+  }
+  
+  onSeccionChange() {
+    this.alumnos = this.alumnosPorSeccion[this.seccion] || [];
+    console.log('Sección seleccionada:', this.seccion);
+    console.log('Alumnos:', this.alumnos);
   }
 
 
